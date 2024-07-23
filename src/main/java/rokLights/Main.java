3f2410847d28b9b65c53ec24032d55d43d1d070f
Rok Nikolič <rok.nikolic@gmail.com>
2024-07-23 15:30:07 +0200

    added example usage

--- src/main/java/rokLights/Main.java
+++ src/main/java/rokLights/Main.java
@@ -1,7 +1,15 @@
 package rokLights;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        LinearAlgebraSolver linSolver = new LinearAlgebraSolver();
+        Game game = new Game();
+        // Example usage
+        int[][] positionToSolve = {{1, 1, 0}, {1, 0, 0}, {0, 1, 0}};
+        game.setProblem(positionToSolve);
+        linSolver.solve(game);
+        System.out.println(Arrays.deepToString(game.getSolution()));
     }
 }
